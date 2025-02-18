08463a121a9d6aef20c966852899f4541dcc1656
TardiGab <tardigab@gmail.com>
2025-02-18 16:38:12 +0100

    Added draggable stickers to header

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,1 +1,11 @@
-console.info('Hello world');
+import gsap from "gsap";
+import Draggable from "gsap/src/Draggable";
+
+gsap.registerPlugin(Draggable);
+
+"use struct";
+
+Draggable.create(".draggable", {
+  bounds: ".header",
+  type: "x,y",
+});
